c9e6cf3e0d065c743117c0393b9f939b55d15dea
Yago Alexandre <goltarayago@gmail.com>
2023-06-14 00:08:52 -0300

    commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gamestar</title>
+    <link rel="shortcut icon" href="img/controller-icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+</body>
+</html>
